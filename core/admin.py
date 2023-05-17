4e30a201cadd9b7d7f499df65e7c3c8e1abf9f88
Matías Ruiz <ma.ruizsa@duocuc.cl>
2023-05-17 10:48:02 -0400

    base de datos movida a core

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Transaccion, Membresia, Cliente
+
+admin.site.register(Transaccion)
+admin.site.register(Membresia)
+admin.site.register(Cliente)
--- database/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Transacciones, Membresia, Cliente
-
-admin.site.register(Transacciones)
-admin.site.register(Membresia)
-admin.site.register(Cliente)
